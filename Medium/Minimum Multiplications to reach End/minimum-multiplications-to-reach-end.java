51f35427245755c342005b61e695f88763ef78b2
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2023-09-07 08:16:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            Solution ob = new Solution();
+            int ans = ob.minimumMultiplications(a, start, end);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minimumMultiplications(int[] arr, int start, int end) {
+      int n = arr.length;
+        int[] ans = new int[100001];
+        
+        Arrays.fill(ans, -1);
+        int mod = 100000;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start % mod);
+        ans[start] = 0;
+
+        
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+            
+            if (temp == end) return ans[end];
+
+            for (int i = 0; i < n; i++) {
+                int toPush = (temp%mod * arr[i]%mod)%mod ;
+
+                // if not visited push it to queue
+                if (ans[toPush] == -1) {
+                    ans[toPush] = ans[temp] + 1;
+                    q.add(toPush);
+                }
+            }
+        }
+
+        return -1;
+    }
+}
