03864cedc3dca90780e119ba11c051f3e813f698
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2024-08-15 00:28:00 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.java
@@ -0,0 +1,88 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GfG {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data);
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+            Solution obj = new Solution();
+            head = obj.addOne(head);
+            printList(head);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public Node addOne(Node head) {
+        // code here.
+        int car = add(head.next);
+        if (car==1)
+        {
+            head.data+=1;
+        }
+        return head;
+    }
+    int add(Node head)
+    {
+        int car = 0;
+        if(head.next == null)
+        {
+            head.data += 1;
+            
+        }
+        else
+        {
+            head.data+=add(head.next);
+            
+        }
+        
+        if(head.data>9)
+        {
+            head.data = head.data%10;
+            car = 1;
+        }
+        return car;
+    }
+}
