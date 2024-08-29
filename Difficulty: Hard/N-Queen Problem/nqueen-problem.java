f479a634ed0f7da2399ebc40a1e5a0c547143ff6
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2024-08-30 00:08:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(in.readLine());
+
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> ans = ob.nQueen(n);
+            if (ans.size() == 0)
+                System.out.println("-1");
+            else {
+                for (int i = 0; i < ans.size(); i++) {
+                    System.out.print("[");
+                    for (int j = 0; j < ans.get(i).size(); j++)
+                        System.out.print(ans.get(i).get(j) + " ");
+                    System.out.print("] ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public ArrayList<ArrayList<Integer>> nQueen(int n) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList();
+        nextQ(n, new int[n], 0, res);
+        return res;
+    }
+    
+    public void nextQ(int n, int[] curr, int done, ArrayList<ArrayList<Integer>> res) {
+        if(done == n) {
+            ArrayList<Integer> intList = new ArrayList<Integer>(curr.length);
+            for (int i : curr)
+                intList.add(i+1);
+            res.add(intList);
+            return;
+        }
+        for(int i=0; i<n; i++) {
+            boolean rejected = false;
+            for(int j=0; j<done && !rejected; j++) { 
+                if(i == curr[j] || Math.abs(curr[j]-i) == (done - j)) {
+                    rejected = true;
+                    break;
+                }
+            }
+            if(!rejected) {
+                curr[done] = i;
+                nextQ(n, curr, done + 1, res);
+            }
+        }
+    }
+}
