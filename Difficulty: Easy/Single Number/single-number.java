0a14b841c5e32b6036d7533ba157457ddef3b1c4
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2024-10-18 07:54:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Single Number/single-number.java
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+// User function Template for Java
+
+class Solution {
+    int getSingle(int arr[]) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int a:arr){
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        for (Map.Entry<Integer, Integer> set :
+             map.entrySet()) {
+            if(set.getValue()%2!=0){
+                return set.getKey();
+            }
+        }
+        return -1;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            // int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            Solution obj = new Solution();
+            int res = obj.getSingle(arr);
+
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
