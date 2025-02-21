41c5274a87c842230e88c0dc29ca8d313dbb8b21
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-02-21 09:17:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.java
@@ -1,62 +1,53 @@
 //{ Driver Code Starts
-import java.util.*;
 import java.io.*;
 import java.lang.*;
+import java.util.*;
 
-class Driverclass
-{
-    public static void main(String args[])
-    {
+class Driverclass {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        
-        //Reading total number of testcases
-        int t= sc.nextInt();
-        
-        while(t-- >0)
-        {
-            //reading the string
+
+        // Reading total number of testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            // reading the string
             String st = sc.next();
-            
-            //calling ispar method of Paranthesis class 
-            //and printing "balanced" if it returns true
-            //else printing "not balanced"
-            if(new Solution().ispar(st) == true)
-                System.out.println("balanced");
+
+            // calling ispar method of Paranthesis class
+            // and printing "balanced" if it returns true
+            // else printing "not balanced"
+            if (new Solution().isBalanced(st) == true)
+                System.out.println("true");
             else
-                System.out.println("not balanced");
-        
+                System.out.println("false");
+
+            System.out.println("~");
         }
     }
 }
 // } Driver Code Ends
 
-
-
-class Solution
-{
-    // Function to return if the paranthesis are balanced or not
-    static boolean ispar(String str)
-    {
-        // add your code here
-        ArrayDeque<Character> st = new ArrayDeque<>();
-
-		for ( int i =0 ;  i <str.length() ; i++){
-			if(str.charAt(i)=='('  || str.charAt(i) =='['  || str.charAt(i) =='{'  )
-				st.push(str.charAt(i));
-
-			else{
-				if(st.isEmpty()) return false;   // too handle str =")" , "(()))" else throws exception.			
-
-				//not empty
-				else if( (str.charAt(i) ==')' && st.pop()=='(') ||
-					(str.charAt(i) ==']' && st.pop()=='[') ||
-					(str.charAt(i) =='}' && st.pop()=='{') )
-			    continue;
-
-				else return false;   // when str.charAt(i) is closing say } but pop is opening but not same type say (, [, in this case directly return false;
-				// Eg: {(}))}
-			}
-		}
-	return(st.isEmpty());
+class Solution {
+    static boolean isBalanced(String s) {
+        // code here
+        Stack<Character> st = new Stack<>();
+        char[] chs = s.toCharArray();
+        
+        
+        for(char ch : chs)
+        {
+            if(ch == '[' || ch == '{' || ch == '(')
+            st.push(ch);
+            
+            else if (!st.isEmpty() && ((ch == ']' && st.peek() == '[') || (ch == '}' && st.peek() == '{') || (ch == ')' && st.peek() == '(')))
+            st.pop();
+            
+            else
+            return false;
+    
+        }
+        return st.isEmpty();
     }
 }
+
