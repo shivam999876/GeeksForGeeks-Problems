54673530f988fb58f0ccb2cccb35072b72be17d0
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-03-15 08:23:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.java
+++ Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.java
@@ -9,41 +9,29 @@ import java.util.*;
 
 class Solution {
 
-   public int minCoins(int coins[], int sum) {
-    // Filter out invalid coins (e.g., 0) to avoid issues
-    coins = Arrays.stream(coins).filter(coin -> coin > 0).toArray();
-    int n = coins.length;
-
-
-    // DP table for storing results
-    int[][] t = new int[n + 1][sum + 1];
-
-    // Initialize the first row (if no coins are available, it's impossible to form any sum > 0)
-    for (int j = 1; j <= sum; j++) {
-        t[0][j] = Integer.MAX_VALUE - 1; // Set to a high value to indicate impossibility
-    }
-
-    // Initialize the first column (sum = 0 requires 0 coins)
-    for (int i = 0; i <= n; i++) {
-        t[i][0] = 0; // 0 coins are required to form a sum of 0
-    }
-
-    // Fill the DP table
-    for (int i = 1; i <= n; i++) { // Start from the first coin type
-        for (int j = 1; j <= sum; j++) {
-            if (coins[i - 1] <= j) { // Coin can be included
-                t[i][j] = Math.min(t[i - 1][j], 1 + t[i][j - coins[i - 1]]);
-            } else { // Coin cannot be included
-                t[i][j] = t[i - 1][j];
+    public int minCoins(int coins[], int sum) {
+        // code here
+        int n = coins.length;
+        int[][] dp = new int[n][sum + 1];
+        for(int i =0; i <= sum; i++) {
+            if(i % coins[0] == 0) {
+                dp[0][i] = i / coins[0];
+            } else {
+                dp[0][i] = (int)(1e9);
             }
         }
+        for(int ind = 1; ind  < n; ind++) {
+            for(int target = 0; target <= sum; target++) {
+                int notTake = dp[ind - 1][target];
+                int take = Integer.MAX_VALUE;
+                if(coins[ind] <= target) {
+                    take = 1 + dp[ind][target - coins[ind]];
+                }
+                dp[ind][target] = Math.min(take, notTake);
+            }
+        }
+        return dp[n-1][sum] >= (int)(1e9) ? -1 : dp[n-1][sum];
     }
-
-    // Final result is in t[n][sum]
-    return t[n][sum] == Integer.MAX_VALUE - 1 ? -1 : t[n][sum];
-}
-
-
 }
 
 
