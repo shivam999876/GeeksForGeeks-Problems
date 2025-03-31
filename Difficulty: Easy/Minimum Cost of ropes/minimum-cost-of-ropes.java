800ceec5485986e94a06dbe5cd6b068a335352b9
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-03-31 08:15:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.java
+++ Difficulty: Easy/Minimum Cost of ropes/minimum-cost-of-ropes.java
@@ -1,44 +1,44 @@
 //{ Driver Code Starts
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine()); // Number of test cases
+        int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
             String[] input = br.readLine().split(" ");
-            long[] a = new long[input.length];
+            int[] a = new int[input.length];
 
             for (int i = 0; i < input.length; i++) {
-                a[i] = Long.parseLong(input[i]);
+                a[i] = Integer.parseInt(input[i]);
             }
 
             Solution ob = new Solution();
             System.out.println(ob.minCost(a));
+            System.out.println("~");
         }
     }
 }
 
 // } Driver Code Ends
 
-
-//Back-end complete function Template for Java
 class Solution {
     // Function to return the minimum cost of connecting the ropes.
-    public long minCost(long[] arr) {
-        // code here
-        long ans = 0;
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        for(Long x:arr)pq.add(x);
-        while(pq.size()!=1){
-            long first = pq.poll();
-            long second = pq.poll();
-            long total = first+second;
-            pq.add(total);
-            ans += total;
+    public int minCost(int[] arr) {
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        for(int i=0;i<arr.length;i++){
+            pq.add(arr[i]);
         }
-        return ans;
+        int cost=0;
+        while(pq.size()>1){
+            int min=pq.remove();
+            int min2=pq.remove();
+            cost+=min+min2;
+            pq.add(min+min2);
+        }
+        return cost;
     }
 }
