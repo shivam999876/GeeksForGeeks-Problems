c5b82a771ab16032cd44d2aa67485d943b38a1bd
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-04-05 08:09:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
@@ -1,61 +1,72 @@
 //{ Driver Code Starts
 import java.util.*;
-import java.lang.*;
-import java.io.*;
+
 class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(s[0]);
-            int m = Integer.parseInt(s[1]);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tc = scanner.nextInt();
+        while (tc-- > 0) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
             char[][] grid = new char[n][m];
+
+            // Read the grid input
             for (int i = 0; i < n; i++) {
-                String[] S = br.readLine().trim().split(" ");
                 for (int j = 0; j < m; j++) {
-                    grid[i][j] = S[j].charAt(0);
+                    grid[i][j] = scanner.next().charAt(0);
                 }
             }
             Solution obj = new Solution();
-            int ans = obj.numIslands(grid);
+            int ans = obj.countIslands(grid);
             System.out.println(ans);
+            System.out.println("~");
         }
+        scanner.close();
     }
 }
+
 // } Driver Code Ends
 
 
-
-
 class Solution {
-    void dfs(int i,int j, char arr[][], int dir[][]){
-        if(i<0 || j<0 || i>=arr.length || j>=arr[0].length || arr[i][j]-'0'==0)return ;
- 
-        arr[i][j]='0';
-        for(int k=0;k<dir.length;k++){
-            int x=i+dir[k][0];
-            int y=j+dir[k][1];
-            dfs(x,y,arr,dir);
-        }
+    int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    private boolean isValid(int i, int j, int n, int m, char[][] grid) {
+        return (i >= 0 && j >= 0 && i < n && j < m && grid[i][j] == 'L');
     }
-    
-  
-    public int numIslands(char[][] arr) {
-        int cnt=0;
-        int n=arr.length;
-        int m=arr[0].length;
-        int dir[][]={{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}};
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(arr[i][j]-'0'==1){
-                    dfs(i,j,arr, dir);
-                    cnt++;
+    public int countIslands(char[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int count = 0;
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 'L') {
+                    count++;
+                    queue.offer(new int[]{i, j});
+                    grid[i][j] = 'S'; 
+
+                    while (!queue.isEmpty()) {
+                        int[] current = queue.poll();
+                        int x = current[0];
+                        int y = current[1];
+
+                        for (int k = 0; k < 8; k++) {
+                            int newX = x + dx[k];
+                            int newY = y + dy[k];
+
+                            if (isValid(newX, newY, n, m, grid)) {
+                                grid[newX][newY] = 's'; 
+                                queue.offer(new int[]{newX, newY});
+                            }
+                        }
+                    }
                 }
             }
         }
-        return cnt;
+
+        return count;
     }
 }
