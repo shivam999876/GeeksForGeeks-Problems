607fe03a0db2794e437b585f386069c7741e6c56
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-04-14 17:46:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Alien Dictionary/alien-dictionary.java
+++ Difficulty: Hard/Alien Dictionary/alien-dictionary.java
@@ -1,123 +1,136 @@
 //{ Driver Code Starts
-/*package whatever //do not write package name here */
-
-import java.io.*;
-import java.math.*;
+// Initial Template for Java
 import java.util.*;
 
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int t = Integer.parseInt(sc.next());
-        while (t-- > 0) {
-            int n = Integer.parseInt(sc.next());
-            int k = Integer.parseInt(sc.next());
-
-            String[] words = new String[n];
-
-            for (int i = 0; i < n; i++) {
-                words[i] = sc.next();
-            }
-
-            Solution ob = new Solution();
-            //  System.out.println(T.findOrder(words,k));
-            String order = ob.findOrder(words, n, k);
-            if (order.length() == 0) {
-                System.out.println(0);
-                continue;
-            }
-            if (order.length() != k) {
-                System.out.println("INCOMPLETE");
-                return;
-            }
-            String temp[] = new String[n];
-            for (int i = 0; i < n; i++) temp[i] = words[i];
-
-            Arrays.sort(temp, new Comparator<String>() {
-                @Override
-                public int compare(String a, String b) {
-                    int index1 = 0;
-                    int index2 = 0;
-                    for (int i = 0;
-                         i < Math.min(a.length(), b.length()) && index1 == index2;
-                         i++) {
-                        index1 = order.indexOf(a.charAt(i));
-                        index2 = order.indexOf(b.charAt(i));
-                    }
-
-                    if (index1 == index2 && a.length() != b.length()) {
-                        if (a.length() < b.length())
-                            return -1;
-                        else
-                            return 1;
-                    }
-
-                    if (index1 < index2)
-                        return -1;
-                    else
-                        return 1;
-                }
-            });
-
-            int flag = 1;
-            for (int i = 0; i < n; i++) {
-                if (!words[i].equals(temp[i])) {
-                    flag = 0;
-                    break;
-                }
-            }
-
-            System.out.println(flag);
-        }
-    }
-}
 
 // } Driver Code Ends
 
 
+
 // User function Template for Java
 
-class Solution
-{
-    public List<Integer> topologicalSort(int k,List<Set<Integer>> al){
-        int indeg[]=new int[k];
-        for(Set<Integer> a:al) for(int i:a) indeg[i]++;
-        Queue<Integer> q=new LinkedList<>();
-        List<Integer> res=new ArrayList<>(); 
-        for(int i=0;i<k;i++) if(indeg[i]==0) q.add(i); 
-        while(!q.isEmpty()){
-            int curr=q.poll();
-            res.add(curr); 
-            for(int i:al.get(curr)){
-                indeg[i]--;
-                if(indeg[i]==0) q.add(i);
+class Solution {
+    public String findOrder(String[] words) {
+        Map<Character, List<Character>> graph = new HashMap<>();
+        Map<Character, Integer> inDegree = new HashMap<>();
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                graph.putIfAbsent(c, new ArrayList<>());
+                inDegree.putIfAbsent(c, 0);
             }
-        } 
-        return res.size()==k?res:new ArrayList<>();
-    }
-    public String findOrder(String [] dict, int N, int K)
-    {
-        // Write your code here
-        List<Set<Integer>> al=new ArrayList<>();
-        for(int i=0;i<K;i++) al.add(new HashSet<>());
-        for(int i=0;i<N-1;i++){
-            String a=dict[i],b=dict[i+1];
-            int sm=Math.min(a.length(),b.length());
-            boolean foundDifference=false;
-            for(int j=0;j<sm;j++){
-                int u=a.charAt(j)-'a',v=b.charAt(j)-'a';
-                if(a.charAt(j)!=b.charAt(j)){
-                    if(!al.get(u).contains(v)) al.get(u).add(v);
-                    foundDifference=true;
+        }
+        for (int i = 0; i < words.length - 1; i++) {
+            String word1 = words[i];
+            String word2 = words[i + 1];
+            int len = Math.min(word1.length(), word2.length());
+            boolean foundDifference = false;
+            
+            for (int j = 0; j < len; j++) {
+                char c1 = word1.charAt(j);
+                char c2 = word2.charAt(j);
+                if (c1 != c2) {
+                    graph.get(c1).add(c2);
+                    inDegree.put(c2, inDegree.get(c2) + 1);
+                    foundDifference = true;
                     break;
                 }
             }
-            if(!foundDifference && a.length()>b.length()) return "";
+            if (!foundDifference && word1.length() > word2.length()) {
+                return "";
+            }
         }
-        List<Integer> sl=topologicalSort(K,al);
-        StringBuilder sb=new StringBuilder();
-        for(int i:sl) sb.append((char)(i+'a'));
-        return sb.toString();
+        Queue<Character> queue = new LinkedList<>();
+        for (char c : inDegree.keySet()) {
+            if (inDegree.get(c) == 0) {
+                queue.add(c);
+            }
+        }
+        StringBuilder order = new StringBuilder();
+        while (!queue.isEmpty()) {
+            char c = queue.poll();
+            order.append(c);
+            for (char neighbor : graph.get(c)) {
+                inDegree.put(neighbor, inDegree.get(neighbor) - 1);
+                if (inDegree.get(neighbor) == 0) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+        if (order.length() != inDegree.size()) {
+            return "";
+        }
+        return order.toString();
     }
 }
+
+
+//{ Driver Code Starts.
+
+public class GFG {
+    private static boolean validate(String[] original, String order) {
+        Map<Character, Integer> mp = new HashMap<>();
+        for (String word : original) {
+            for (char ch : word.toCharArray()) {
+                mp.put(ch, 1);
+            }
+        }
+        for (char ch : order.toCharArray()) {
+            if (!mp.containsKey(ch)) {
+                return false;
+            }
+            mp.remove(ch);
+        }
+        if (!mp.isEmpty()) {
+            return false;
+        }
+
+        Map<Character, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < order.length(); i++) {
+            indexMap.put(order.charAt(i), i);
+        }
+
+        for (int i = 0; i < original.length - 1; i++) {
+            String a = original[i];
+            String b = original[i + 1];
+            int k = 0, n = a.length(), m = b.length();
+
+            while (k < n && k < m && a.charAt(k) == b.charAt(k)) {
+                k++;
+            }
+
+            if (k < n && k < m &&
+                indexMap.get(a.charAt(k)) > indexMap.get(b.charAt(k))) {
+                return false;
+            }
+            if (k != n && k == m) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine()); // Number of test cases
+
+        while (t-- > 0) {
+            String[] words = sc.nextLine().split(" ");
+            String[] original = Arrays.copyOf(words, words.length);
+
+            Solution ob = new Solution();
+            String order = ob.findOrder(words);
+
+            if (order.isEmpty()) {
+                System.out.println("\"\"");
+            } else {
+                System.out.println(validate(original, order) ? "true" : "false");
+            }
+            System.out.println("~");
+        }
+
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
