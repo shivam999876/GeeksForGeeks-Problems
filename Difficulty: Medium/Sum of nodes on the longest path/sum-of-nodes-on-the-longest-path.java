9abb8b56d8ab6a1a345e2c34aab9bf0b2d9bd68b
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-05-29 20:44:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of nodes on the longest path/sum-of-nodes-on-the-longest-path.java
@@ -0,0 +1,44 @@
+
+
+//User function Template for Java
+
+/*
+node class of binary tree
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution{
+    int maxSum = Integer.MIN_VALUE;
+    int maxLength = 0;
+
+    public int sumOfLongRootToLeafPath(Node root) {
+        fun(root, 0, 0);
+        return maxSum;
+    }
+
+    private void fun(Node node, int pathLength, int pathSum) {
+        if (node == null) //bc1
+            return;
+
+        pathLength++;
+        pathSum += node.data;
+
+       //bc2 - leaf node
+        if (node.left == null && node.right == null) {
+            if (pathLength > maxLength || (pathLength == maxLength && pathSum > maxSum)) {
+                maxLength = pathLength;
+                maxSum = pathSum;
+            }
+        }
+
+       //recur
+        fun(node.left, pathLength, pathSum);
+        fun(node.right, pathLength, pathSum);
+        }
+}
