90268e5261293f0e26446e1db167e4d2f448fa01
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-06-01 14:54:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count pairs Sum in matrices/count-pairs-sum-in-matrices.java
@@ -0,0 +1,38 @@
+
+
+//User function Template for Java
+
+class Solution {
+    int countPairs(int[][] mat1, int[][] mat2, int x) {
+        // code here
+        int n=mat1.length;
+
+         int count =0;
+         
+         HashSet<Integer> set=new HashSet<>();
+         
+         for(int i=0;i<n;i++)
+         {
+             for(int j=0;j<n;j++)
+             {
+                 set.add(mat2[i][j]);
+             }
+         }
+         
+         for(int i=0;i<n;i++)
+         {
+             for(int j=0;j<n;j++)
+             {
+                 int val=mat1[i][j];
+                 
+                 int ans=x-val;
+                 
+                 if(set.contains(ans))
+                 {
+                     count ++;
+                 }
+             }
+         }
+         return count;
+    }
+}
