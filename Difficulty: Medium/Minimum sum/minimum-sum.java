8a52e3021c19ea1c872f2b88e9e98915fe1c8740
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-06-23 20:43:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum sum/minimum-sum.java
+++ Difficulty: Medium/Minimum sum/minimum-sum.java
@@ -1,31 +1,5 @@
-//{ Driver Code Starts
-// Initial Template for Java
 
-import java.io.*;
-import java.lang.*;
-import java.util.*;
 
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-
-        while (t-- > 0) {
-            String arr[] = br.readLine().split(" ");
-            int a[] = new int[arr.length];
-
-            for (int i = 0; i < arr.length; i++) {
-                a[i] = Integer.parseInt(arr[i]);
-            }
-            Solution obj = new Solution();
-            int f = 0;
-            String A = obj.minSum(a);
-            System.out.println(A);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
 
 
 // User function Template for Java
