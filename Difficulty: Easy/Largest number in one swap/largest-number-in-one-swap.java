cabcafe0a3f6d8824bcebeceb88ef62885eccf36
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-09-10 08:07:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.java
+++ Difficulty: Easy/Largest number in one swap/largest-number-in-one-swap.java
@@ -1,49 +1,25 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t-- > 0) {
-            String s = read.readLine();
-            Solution ob = new Solution();
-            System.out.println(ob.largestSwap(s));
-
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    String largestSwap(String s) {
-        char[] arr = s.toCharArray();
-        int n = arr.length;
-        int maxIdx = n - 1;
-        int swapIdx1 = -1, swapIdx2 = -1;
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (arr[i] < arr[maxIdx]) {
-                swapIdx1 = i;
-                swapIdx2 = maxIdx;
-            } else if (arr[i] > arr[maxIdx]) {
-                maxIdx = i;
+    public String largestSwap(String s) {
+        // code here
+        char maxVal = '0';
+        int valIdx = -1;
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+                if(c >= maxVal){
+                    maxVal = c;
+                    valIdx = i;
+                }
             }
+            StringBuilder result = new StringBuilder(s);
+            for(int i = 0; i < s.length(); i++){
+                char c = s.charAt(i);
+                if(i != valIdx && maxVal != c){
+                    char d = result.charAt(i);
+                    result.setCharAt(i, maxVal);
+                    result.setCharAt(valIdx,d);
+                    break;
+                }
+            }
+            return result.toString().compareTo(s) >= 0 ? result.toString() : s;
         }
-
-        if (swapIdx1 != -1) {
-            char temp = arr[swapIdx1];
-            arr[swapIdx1] = arr[swapIdx2];
-            arr[swapIdx2] = temp;
-        }
-        
-        return new String(arr);
     }
-}
