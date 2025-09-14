ed215d7a13f7ad073480cd2711177db639cf77a5
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-09-14 19:14:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Gas Station/gas-station.java
+++ Difficulty: Medium/Gas Station/gas-station.java
@@ -1,55 +1,7 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.lang.*;
-import java.util.*;
-
-class Main {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-
-        while (t-- > 0) {
-
-            ArrayList<Integer> array1 = new ArrayList<Integer>();
-            ArrayList<Integer> array2 = new ArrayList<Integer>();
-
-            String line = read.readLine();
-            String[] tokens = line.split(" ");
-            for (String token : tokens) {
-                array1.add(Integer.parseInt(token));
-            }
-            line = read.readLine();
-            tokens = line.split(" ");
-            for (String token : tokens) {
-                array2.add(Integer.parseInt(token));
-            }
-
-            // ArrayList<Integer> v = new ArrayList<Integer>();
-            int[] gas = new int[array1.size()];
-            int idx = 0;
-            for (int i : array1) gas[idx++] = i;
-
-            int[] cost = new int[array2.size()];
-            idx = 0;
-            for (int i : array2) cost[idx++] = i;
-
-            int ans = new Solution().startStation(gas, cost);
-
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int startStation(int[] gas, int[] cost) {
-        int n = cost.length;
+        // code here
+          int n = cost.length;
         int balance = 0, deficit = 0, start = 0;
         
         for(int i=0; i<n; i++) {
