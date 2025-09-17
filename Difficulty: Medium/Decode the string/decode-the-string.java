fe3e114f43d928cd00309f09310b127746fd2b04
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-09-17 08:36:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Decode the string/decode-the-string.java
+++ Difficulty: Medium/Decode the string/decode-the-string.java
@@ -1,67 +1,37 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out = new PrintWriter(System.out);
-        int t = Integer.parseInt(in.readLine());
-        while (t-- > 0) {
-            String s = in.readLine();
-
-            Solution ob = new Solution();
-            out.println(ob.decodeString(s));
-
-            out.println("~");
-        }
-        out.close();
-    }
-}
-// } Driver Code Ends
-
-
-
 class Solution {
     static String decodeString(String s) {
         // code here
-        Stack<Integer>val=new Stack<>();
-        Stack<Character>stack=new Stack<>();
-        StringBuilder res=new StringBuilder();
-        for(int i=0;i<s.length();){
-            char ch=s.charAt(i);
-            if(ch>='0'&&ch<='9'){
-                StringBuilder str=new StringBuilder();
-                while(ch>='0'&&ch<='9'){
-                    str.append(ch);
-                    i++;
-                    ch=s.charAt(i);
+        Stack<String> stack = new Stack();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(Character.isDigit(c)){
+                int start = i;
+                i += 1;
+                while(Character.isDigit(s.charAt(i))) i++;
+                stack.push(s.substring(start, i));
+                i--;
+                continue;
+            }
+            if(c == ']'){
+                StringBuilder expandedString = new StringBuilder();
+                String temp;
+                while(!(temp = stack.pop()).equals("[")){
+                    expandedString.insert(0, temp);
                 }
-                int cnt=Integer.parseInt(str.toString());
-                val.push(cnt);
-            }else if(ch==']'){
-                StringBuilder str=new StringBuilder();
-                while(!stack.isEmpty()&&stack.peek()!='['){
-                    char ch1=stack.pop();
-                    str.append(ch1);
+                 int count = Integer.parseInt(stack.pop());
+                StringBuilder finalString = new StringBuilder();
+                while(count-- > 0){
+                    finalString.append(expandedString);
                 }
-                stack.pop();
-                int times=val.pop();
-                str.reverse();
-                for(int j=0;j<times;j++){
-                    for(int k=0;k<str.length();k++){
-                        stack.push(str.charAt(k));
-                    }
-                }
-                i++;
+                stack.push(finalString.toString());
             }else{
-                stack.push(ch);
-                i++;
+                stack.push(String.valueOf(c));
             }
         }
-       while (!stack.isEmpty()) {
-            res.append(stack.pop());
+        StringBuilder solution = new StringBuilder();
+        while(!stack.isEmpty()){
+            solution.insert(0, stack.pop());
         }
-        return res.reverse().toString();
+        return solution.toString();
     }
 }
