69732578c3fb131870d5ed0d140ab8fc4f3481b7
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-09-29 13:48:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Maximum subarray sum 2/maximum-subarray-sum-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxSubarrSum(int[] arr, int a, int b) {
+     int n = arr.length;
+        long[] prefix = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + arr[i];
+        }
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        long ans = Long.MIN_VALUE;
+
+        for (int r = a; r <= n; r++) {
+            int idx = r - a;
+            while (!dq.isEmpty() && prefix[dq.peekLast()] >= prefix[idx]) {
+                dq.pollLast();
+            }
+            dq.addLast(idx);
+            while (!dq.isEmpty() && dq.peekFirst() < r - b) {
+                dq.pollFirst();
+            }
+            if (!dq.isEmpty()) {
+                ans = Math.max(ans, prefix[r] - prefix[dq.peekFirst()]);
+            }
+        }
+
+        return (int) ans;
+    }
+}
