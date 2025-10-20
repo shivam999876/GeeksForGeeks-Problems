ebcf7efa1cfe168f7a26cef106fba2ef8cce5594
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-10-20 09:17:05 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Number of BST From Array/number-of-bst-from-array.java
@@ -0,0 +1,38 @@
+class Solution {
+    private static int mem[] = new int[30];
+    private static int findCatalan(int n){
+        if(n <= 1){
+            return 1;
+        }
+        if(mem[n] != 0) return mem[n];
+        int res = 0;
+        for(int i = 0; i < n; i++){
+            res += findCatalan(i) * findCatalan(n - i - 1);
+        }
+        return mem[n] = res;
+    }
+    private int countLess(int arr[], int x){
+        int c = 0;
+        for(int e : arr){
+            if(e < x) c++;
+        }
+        return c;
+    }
+    private int countMore(int arr[], int x){
+        int c = 0;
+        for(int e : arr){
+            if(e > x) c++;
+        }
+        return c;
+    }
+    public ArrayList<Integer> countBSTs(int[] arr) {
+        // Code here
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int x : arr){
+            int less = countLess(arr, x);
+            int more = countMore(arr, x);
+            ans.add(findCatalan(less) * findCatalan(more));
+        }
+        return ans;
+    }
+}
