52832d5b3f77457d7b5d45a101e185b594c81943
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-12-22 21:11:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.java
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.java
@@ -1,51 +1,18 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt();
-        while (tc-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int arr[][] = new int[n][m];
-
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    arr[i][j] = sc.nextInt();
-                }
-            }
-            int ans = new Solution().rowWithMax1s(arr);
-            System.out.println(ans);
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-
-
 // User function Template for Java
 
 class Solution {
-public int rowWithMax1s(int arr[][]) {
+    public int rowWithMax1s(int arr[][]) {
         // code here
-        int i=0,j=arr[0].length-1,ans=-1;
-        while(i<arr.length && j>=0){
-            if(arr[i][j]==0){
-                i++;
-            }
-            else{
-                ans=i;
+        int n = arr.length;
+        int j = arr[0].length - 1;
+        int ans = -1;
+        for(int i = 0; i < n; i++){
+            while(j >= 0 && arr[i][j] == 1){
+                ans = i;
                 j--;
             }
+            if(j == -1) return i;
         }
         return ans;
     }
-    
 }
