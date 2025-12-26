efdd74c6b92274a8ff1f0a0d1326ae7dd778faed
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2025-12-26 20:24:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
+++ Difficulty: Medium/Kth Missing Positive Number in a Sorted Array/kth-missing-positive-number-in-a-sorted-array.java
@@ -1,50 +1,16 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.kthMissing(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     public int kthMissing(int[] arr, int k) {
         // code here
-    int start=0;
-    int end=arr.length-1;
-    while(start<=end){
-        int mid=(start+end)/2;
-        int numberOfMissingElements=arr[mid]-(mid+1);
-        if(numberOfMissingElements<k){
-            start=mid+1;
+        ArrayList<Integer> res = new ArrayList<>();
+        int j = 0;
+        int l = arr[arr.length - 1];
+        for(int i = 1; res.size() < k; i++){
+            if(j < arr.length && arr[j] == i){
+                j++;
+            }else{
+                res.add(i);
+            }
         }
-        else{
-           end=mid-1; 
-        }
-    }
-    return k+start;
+        return res.get(k - 1);
     }
 }
-
