78b60e2142a4bef609bc2d3e8854376091a6250c
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-01-07 18:20:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count distinct elements in every window/count-distinct-elements-in-every-window.java
@@ -0,0 +1,20 @@
+
+
+class Solution {
+     ArrayList<Integer> countDistinct(int arr[], int k) {
+        // code here
+        int n=arr.length,j=0;
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashMap<Integer,Integer> frequency = new HashMap<>();
+        for(int i=0;i<n;i++){
+            frequency.put(arr[i],frequency.getOrDefault(arr[i],0)+1);
+            if(i>=k-1){
+                ans.add(frequency.size());
+                frequency.put(arr[j],frequency.get(arr[j])-1);
+                if(frequency.get(arr[j])==0)frequency.remove(arr[j]);
+                j++;
+            }
+        }
+        return ans;
+    }
+}
