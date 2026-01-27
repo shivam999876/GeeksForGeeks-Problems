f0733b86c58663a0d5a4d4735471a3f871ab9068
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-01-27 21:09:43 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Word Search/word-search.java
+++ Difficulty: Medium/Word Search/word-search.java
@@ -1,37 +1,5 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
 
-class GFG {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tc = sc.nextInt(); // Number of test cases
-        while (tc-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            char[][] mat = new char[n][m];
 
-            // Reading the matrix
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    mat[i][j] = sc.next().charAt(0);
-                }
-            }
-
-            String word = sc.next();
-            Solution obj = new Solution();
-            boolean ans = obj.isWordExist(mat, word);
-            if (ans)
-                System.out.println("true");
-            else
-                System.out.println("false");
-
-            System.out.println("~");
-        }
-        sc.close();
-    }
-}
-// } Driver Code Ends
 
 
 
