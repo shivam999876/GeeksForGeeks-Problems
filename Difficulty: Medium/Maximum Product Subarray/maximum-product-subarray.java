8d104441e2c40e8c1f8f5de60a4e065a6fdce63f
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-02-08 19:22:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
+++ Difficulty: Medium/Maximum Product Subarray/maximum-product-subarray.java
@@ -1,29 +1,4 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine());
-        while (tc-- > 0) {
-            String[] inputLine = br.readLine().split(" ");
-            int n = inputLine.length;
-            int[] arr = new int[n];
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-            System.out.println(new Solution().maxProduct(arr));
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
-    // Function to find maximum product subarray
     int maxProduct(int[] arr) {
         // code here
         int ans =Integer.MIN_VALUE,productStart = 1, productEnd = 1, n = arr.length;
