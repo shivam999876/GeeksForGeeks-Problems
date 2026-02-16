6f869c048a45646d80ef0c9907eb80e4d4eec66a
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-02-16 19:11:16 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.java
@@ -0,0 +1,13 @@
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        // code here
+        Arrays.sort(arr, (a,b) -> Integer.compare(a[0],b[0]));
+        int end = arr[0][1];
+        for(int i=1; i < arr.length; i++){
+            if(end > arr[i][0])
+            return false;
+            end = arr[i][1];
+        }
+        return true;
+    }
+}
