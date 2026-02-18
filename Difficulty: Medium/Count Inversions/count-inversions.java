cd39c4d4f05e42a9bbb8aac71d41c9ad2dda6eb5
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-02-18 11:58:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Inversions/count-inversions.java
+++ Difficulty: Medium/Count Inversions/count-inversions.java
@@ -1,27 +1,4 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorting {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        for (int g = 0; g < t; g++) {
-            String[] str = (br.readLine()).trim().split(" ");
-            int arr[] = new int[str.length];
-            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
-            System.out.println(new Solution().inversionCount(arr));
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
-    // Function to count inversions in the array.
     static int inversionCount(int arr[]) {
         // Your Code Here
         int[] temp=new int[arr.length];
