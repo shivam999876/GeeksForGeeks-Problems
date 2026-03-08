bde4351f566315896cf2f40e05ca94aecedd277d
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-03-08 15:50:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.java
+++ Difficulty: Medium/Pythagorean Triplet/pythagorean-triplet.java
@@ -1,7 +1,7 @@
 class Solution {
     boolean pythagoreanTriplet(int[] arr) {
         // code here
-        int n=arr.length;
+         int n=arr.length;
         HashSet<Integer> set=new HashSet<>();
         for(int i=0;i<n;i++){
             int sq=arr[i]*arr[i];
