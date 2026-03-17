3ea273aed25624a5c2bf4e363a93920cf58fb702
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-03-17 21:08:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.java
+++ Difficulty: Hard/Burning Tree/burning-tree.java
@@ -1,64 +1,87 @@
+/*
+class Node {
+    int data;
+    Node left;
+    Node right;
 
-
-//User function Template for Java
-
-class Solution
-{
-    /*class Node {
-    	int data;
-    	Node left;
-    	Node right;
-    
-    	Node(int data) {
-    		this.data = data;
-    		left = null;
-    		right = null;
-    	}
-    }*/
-    static int timeTaken;
-    public static int minTime(Node root, int target) 
-    {
-        timeTaken=0;
-        solve(root,target);
-        return timeTaken;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
     }
+}
+*/
+
+class Pair {
+    int node;
+    Node left, right, parent;
+    public Pair(int n, Node l, Node r, Node p) {
+        this.node = n;
+        this.left = l;
+        this.right = r;
+        this.parent = p;
+    }
+}
+
+class Solution {
     
-    static Info solve(Node root,int target){
-        if(root==null)
-        return new Info(false,-1);
-        Info left=solve(root.left,target);
-        Info right=solve(root.right,target);
+    HashMap<Integer, Pair> map = new HashMap<>();
+    
+    public void dfs(Node node, Node parent) {
+        if(node == null) return;
         
-        int max=1+Math.max(left.height,right.height);
-        //timeTaken=Math.max(timeTaken,max);
-        if(root.data==target){
-            // check and update height with children
-            timeTaken=Math.max(timeTaken,max);
-            right.fireSet=true;
-            right.height=0;
-            return right;
-        }
-        if(left.fireSet){
-            timeTaken=Math.max(timeTaken,left.height+right.height+2);
-            left.height+=1;
-            return left;
-        }
-        if(right.fireSet){
-            timeTaken=Math.max(timeTaken,left.height+right.height+2);
-            right.height+=1;
-            return right;
-        }
-        left.height=max;
-        return left;
+        Node left = node.left == null ? null : node.left;
+        Node right = node.right == null ? null : node.right;
+        
+        map.put(node.data, new Pair(node.data, left, right, parent));
+        
+        dfs(node.left, node);
+        dfs(node.right, node);
+        
     }
     
-    
-    static class Info{
-        boolean fireSet;
-        int height;
-        Info(boolean fireSet,int height){
-            this.fireSet=fireSet;
-            this.height=height;
+    public int minTime(Node root, int target) {
+        map = new HashMap<>();
+        dfs(root, null);
+
+        Queue<Pair> q = new LinkedList<>();
+
+        q.add(new Pair(target, map.get(target).left, map.get(target).right, map.get(target).parent));
+
+        int time = 0;
+
+        HashSet<Integer> visited = new HashSet<>();
+        visited.add(target);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                Pair curr = q.remove();
+                int data = curr.node;
+                Node left = map.get(data).left, right = map.get(data).right, parent = map.get(data).parent;
+
+                if (left != null && !visited.contains(left.data)) {
+                    visited.add(left.data);
+                    q.add(map.get(left.data));
+                }
+
+                if (right != null && !visited.contains(right.data)) {
+                    visited.add(right.data);
+                    q.add(map.get(right.data));
+                }
+
+                if (parent != null && !visited.contains(parent.data)) {
+                    visited.add(parent.data);
+                    q.add(map.get(parent.data));
+                }
+            }
+
+            time++;
+
         }
+
+        return time - 1;
+        
     }
 }
