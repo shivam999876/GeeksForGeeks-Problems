7fae1d33f126b96ffb90615105a2266e6b1b33a3
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-06-08 09:15:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete Nodes with Greater on Right/delete-nodes-with-greater-on-right.java
@@ -0,0 +1,55 @@
+/* Structure of linked list node
+class Node {
+
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+*/
+class Solution {
+    Node compute(Node head) {
+        // code here
+        Node cur = head;
+        Node prev = null;
+        Node next;
+        while(cur!=null)
+        {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        head = prev;
+        Node current = head;
+        Node maxNode = head;
+        Node temp;
+         while (current != null && current.next != null)
+         {
+             if(current.next.data < maxNode.data)
+             {
+                 temp = current.next;
+                 current.next = temp.next;
+             }
+             else
+             {
+                 current = current.next;
+                 maxNode= current;
+             }
+         }
+         cur = head;
+         prev = null;
+         while(cur!=null)
+         {
+             next = cur.next;
+             cur.next = prev;
+             prev = cur;
+             cur = next;
+         }
+         head = prev;
+        return head;
+    }
+}
